343d5fe95d816da1c0bf392ef4d33016389f32c9
Muhammad Ibrar <mibrar@ssidecisions.com>
2023-06-03 17:20:49 +0500

    update env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -9,19 +11,22 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { SharedModule } from './shared/shared.module';
 import { Note } from './notes/entity/notes.entity';
 import { NotesModule } from './notes/notes.module';
+dotenv.config();
 
+const ENV = process.env;
+console.log(ENV)
 @Module({
   imports: [
     ScheduleModule.forRoot(),
     TypeOrmModule.forRoot({
-      host: 'localhost',
-      password: 'postgres',
-      database: 'taskDB',
+      host: ENV.DB_HOST,
+      password: ENV.DB_PASSWORD,
+      database: ENV.DB_NAME,
       entities: [User, Note],
       synchronize: true,
       type: 'postgres',
-      port: 5432,
-      username: 'postgres',
+      port: +ENV.DB_PORT,
+      username: ENV.DB_USERNAME,
     }),
 
     AuthModule, 
